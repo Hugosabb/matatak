aebb14c0e366fe9366c4ff9b56360f94ecd6f095
Hugosabb <hugo.sabatier@ensta-bretagne.org>
2026-06-26 15:22:51 +0200

    🐛 BOT icon online

--- client/player.ts
+++ client/player.ts
@@ -4,15 +4,16 @@ import { aiLevel } from './result';
 import { displayName } from './view';
 
 export function player(id: string, title: string, name: string, rating: string, level: number) {
+    const isBotOrGhost = ['BOT', 'GHOST'].includes(title);
     return h('round-' + id, [
         h('div.player-data', [
-            h('i-side#' + id + '.online.icon', { class: { "icon-online": false, "icon-offline": true } }),
+            h('i-side#' + id + '.online.icon', { class: { "icon-online": isBotOrGhost, "icon-offline": !isBotOrGhost } }),
             h('player', [
                 h('a.user-link', { attrs: {href: '/@/' + name} }, [
                     (title !== '') ? h('player-title', title + ' ') : '',
                     displayName(name) + aiLevel(title, level),
                 ]),
-                h('rating', !['BOT', 'GHOST'].includes(title) ? rating : ''),
+                h('rating', !isBotOrGhost ? rating : ''),
             ]),
         ]),
     ]);
