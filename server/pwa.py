3ed74143b74ac78f06194ed9eedb7bc9268eea16
Hugosabb <hugo.sabatier@ensta-bretagne.org>
2026-06-26 10:38:45 +0200

    🐛 Disable service worker cache for updates

--- server/pwa.py
+++ server/pwa.py
@@ -2,7 +2,9 @@ from aiohttp import web
 import os
 
 async def service_worker(request):
-    return web.FileResponse(os.path.join("static", "service-worker.js"))
+    response = web.FileResponse(os.path.join("static", "service-worker.js"))
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    return response
 
 async def manifest(request):
     return web.FileResponse(os.path.join("static", "manifest.json"))
