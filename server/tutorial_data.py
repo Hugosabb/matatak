b6322808e02a01cb7e366a4d04ada80f04f4ce34
Hugosabb <hugo.sabatier@ensta-bretagne.org>
2026-06-26 12:16:53 +0200

    💄 Tutorial update to 4x8 board

--- server/tutorial_data.py
+++ server/tutorial_data.py
@@ -3,7 +3,7 @@ TUTORIAL_STEPS = {
         "id": "1",
         "variant": "matatakmini",
         "title": "Se déplacer",
-        "fen": "4/4/4/4/2E1/4 w - - 0 1",
+        "fen": "4/4/4/4/4/4/2E1/4 w - - 0 1",
         "moves": "c2c3",
         "instruction": "Bienvenue ! Déplace ton pion d'une case vers l'avant (clique dessus, puis sur la case devant lui).",
         "success_msg": "Bravo ! Tu sais maintenant te déplacer."
@@ -12,7 +12,7 @@ TUTORIAL_STEPS = {
         "id": "2",
         "variant": "matatakmini",
         "title": "Capturer",
-        "fen": "4/4/2g1/4/2F1/4 w - - 0 1",
+        "fen": "4/4/4/4/2g1/4/2F1/4 w - - 0 1",
         "moves": "c2c4",
         "instruction": "Les personnages à l'envers sont ceux de ton adversaire. Si tu déplaces ton personnage sur un personnage adverse, tu le captures ! Capture le personnage adverse.",
         "success_msg": "Super ! Tu as capturé un personnage adverse, il est retiré du plateau."
@@ -21,7 +21,7 @@ TUTORIAL_STEPS = {
         "id": "3",
         "variant": "matatakmini",
         "title": "Capturer",
-        "fen": "1k2/1mm1/4/1B1c/2B1/I1K1 w - - 0 1",
+        "fen": "1k2/1mm1/4/4/4/1B1c/2B1/I1K1 w - - 0 1",
         "moves": "c2d3",
         "instruction": "Quand tu cliques sur un personnage, les cases où il pourrait aller en théorie sont indiquées par des losanges, et celles où il peut actuellement aller avec un cercle. Capture le personnage adverse.",
         "success_msg": "Bien joué !"
@@ -30,7 +30,7 @@ TUTORIAL_STEPS = {
         "id": "4",
         "variant": "matatakmini",
         "title": "Capturer",
-        "fen": "1k1m/4/mB2/3B/4/I1K1 w - - 0 1",
+        "fen": "4/4/1k1m/4/mB2/3B/4/I1K1 w - - 0 1",
         "moves": "a1a4",
         "instruction": "Capture un personnage adverse.",
         "success_msg": "Parfait !"
@@ -39,7 +39,7 @@ TUTORIAL_STEPS = {
         "id": "5",
         "variant": "matatakmini",
         "title": "Échec",
-        "fen": "k3/1mm1/4/1B2/2B1/1IK1 w - - 0 1",
+        "fen": "k3/1mm1/4/4/4/1B2/2B1/1IK1 w - - 0 1",
         "moves": "b1a1",
         "instruction": "Quand on menace de capturer un roi, on fait un échec ! Fais un échec au roi.",
         "success_msg": "C'est ça ! Le roi est en échec, l'adversaire est obligé de réagir."
@@ -48,8 +48,8 @@ TUTORIAL_STEPS = {
         "id": "6",
         "variant": "matatakmini",
         "title": "Échec et mat",
-        "fen": "2k1/3m/2B1/4/4/I1K1 w - - 0 1",
-        "moves": "a1a6",
+        "fen": "2k1/3m/2B1/4/4/4/4/I1K1 w - - 0 1",
+        "moves": "a1a8",
         "instruction": "Si le roi attaqué ne peut pas fuir, c'est Echec et Mat ! Fais un échec et mat.",
         "success_msg": "Félicitations, c'est un échec et mat !"
     },
@@ -57,7 +57,7 @@ TUTORIAL_STEPS = {
         "id": "7",
         "variant": "matatakmini",
         "title": "Échec et mat",
-        "fen": "2c1/1m2/km2/2B1/1K2/2I1 w - - 0 1",
+        "fen": "4/2c1/1m2/km2/2B1/1K2/4/2I1 w - - 0 1",
         "moves": "c1a1",
         "instruction": "Trouve le bon coup pour faire un échec et mat.",
         "success_msg": "Bravo !"
@@ -66,8 +66,8 @@ TUTORIAL_STEPS = {
         "id": "8",
         "variant": "matatakmini",
         "title": "Échec",
-        "fen": "1k2/1m2/2m1/1B1c/2IK/4 w - - 0 1",
-        "moves": "d2c1",
+        "fen": "4/1k2/1m2/2m1/1B1c/2IK/4/4 w - - 0 1",
+        "moves": "d3c2",
         "instruction": "Attention, ton roi est menacé (en échec) et il n'a pas le droit d'y rester ! Déplace-le sur une case sûre.",
         "success_msg": "Ouf, ton roi est sauvé ! N'oublie pas : un roi ne peut pas se mettre de lui-même en échec."
     },
@@ -75,8 +75,8 @@ TUTORIAL_STEPS = {
         "id": "9",
         "variant": "matatakmini",
         "title": "Mananas",
-        "fen": "1k2/1cm1/1m2/4/1BK1/1I2 w - - 0 1",
-        "moves": "c2b3",
+        "fen": "4/1k2/1cm1/1m2/4/1BK1/4/1I2 w - - 0 1",
+        "moves": "c3b4",
         "instruction": "Gagner par échec et mat n'est pas la seule option ! Déplace ton Roi sur l'une des 4 cases centrales pour attraper le Mananas et gagner immédiatement.",
         "success_msg": "Génial ! Tu as récupéré le Mananas."
     },
@@ -84,8 +84,8 @@ TUTORIAL_STEPS = {
         "id": "10",
         "variant": "matatakmini",
         "title": "Final",
-        "fen": "4/2k1/m2m/4/2K1/I3 w - - 0 1",
-        "moves": "a1a4,c5b5,c2b3",
+        "fen": "4/4/2k1/m2m/4/2K1/4/I3 w - - 0 1",
+        "moves": "a1a5,c6b6,c3b4",
         "instruction": "Attention le roi adverse menace d'attraper le Mananas ! Gagne la partie en 2 coups !",
         "success_msg": "Bravo tu es prêt à jouer !"
     }
